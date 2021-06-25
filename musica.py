20430a2d1b3da23f5b8d1a4463d0d52f4091cc47
lafith <lafithmattara@gmail.com>
2021-06-25 20:17:24 +0530

    include logger

--- musica.py
+++ musica.py
@@ -1,9 +1,27 @@
 # Script for running MUSICA algorithm on a grayscale image:
 # Written by Lafith Mattara on 2021-06-05
 
+
+import logging
 import numpy as np
 import copy
 from skimage.transform import pyramid_reduce, pyramid_expand
+
+
+logging.getLogger('matplotlib').setLevel(logging.WARNING)
+logger = logging.getLogger(__name__)
+#change level here for console output
+logger.setLevel(logging.INFO)
+
+file_handler = logging.FileHandler('musica_py.log')
+formatter = logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s')
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
+
+stream_handler = logging.StreamHandler()
+stream_format = logging.Formatter('%(levelname)s:\t%(message)s')
+stream_handler.setFormatter(stream_format)
+logger.addHandler(stream_handler)
 
 
 def isPowerofTwo(x):
@@ -40,10 +58,10 @@ def resize_image(img):
     numpy.ndarray
         Resized image after padding
     """
-    print('\nResizing image...')
     row, col = img.shape
     # check if dimensions are power of two
     # if not pad the image accordingly
+    logger.debug("Calculating how much padding is required...")
     if isPowerofTwo(row):
         rowdiff = 0
     else:
@@ -60,6 +78,10 @@ def resize_image(img):
         img,
         ((0, rowdiff), (0, coldiff)),
         'reflect')
+    logger.info(
+            'Image padded from [{},{}] to [{},{}]'.format(
+                img.shape[0], img.shape[1],
+                img_.shape[0],img_.shape[1]))
     return img_
 
 
@@ -78,14 +100,15 @@ def gaussian_pyramid(img, L):
     list
         list containing images from g0 to gL in order
     """
-    print('\nCreating Gaussian pyramid...')
+    logger.debug('Creating Gaussian pyramid...')
     # Gaussian Pyramid
     tmp = copy.deepcopy(img)
     gp = [tmp]
-    for _ in range(L):
-        #print('creating Layer %d...' % (layer+1))
+    for layer in range(L):
+        logger.debug('creating Layer %d...' % (layer+1))
         tmp = pyramid_reduce(tmp, preserve_range=True)
         gp.append(tmp)
+    logger.info('Finished creating Gaussian Pyramid')
     return gp
 
 
@@ -107,15 +130,16 @@ def laplacian_pyramid(img, L):
         list containing layers of gauss pyramid
     """
     gauss = gaussian_pyramid(img, L)
-    print('\nCreating Laplacian pyramid...')
+    logger.debug('Creating Laplacian pyramid...')
     # Laplacian Pyramid:
     lp = []
     for layer in range(L):
-        #print('Creating layer %d' % (layer))
+        logger.debug('Creating layer %d' % (layer))
         tmp = pyramid_expand(gauss[layer+1], preserve_range=True)
         tmp = gauss[layer] - tmp
         lp.append(tmp)
     lp.append(gauss[L])
+    logger.info("Finished creating Laplacian pyramid")
     return lp, gauss
 
 
@@ -154,16 +178,16 @@ def enhance_coefficients(laplacian, L, params):
     list
         List of enhanced pyramid coeffiencts.
     """
-    print('\nNon linear transformation of coefficients...')
+    logger.debug('Non linear transformation of coefficients...')
     # Non linear operation goes here:
     M = params['M']
     p = params['p']
     a = params['a']
     for layer in range(L):
+        logger.info('Modifying Layer %d' % (layer))
         x = laplacian[layer]
         x[x < 0] = 0.0  # removing all negative coefficients
         G = a[layer]*M
-        #print('Modifying Layer %d' % (layer))
         laplacian[layer] = G*np.multiply(
             np.divide(
                 x, np.abs(x), out=np.zeros_like(x),where=x != 0),
@@ -189,13 +213,15 @@ def reconstruct_image(laplacian, L):
     numpy.ndarray
         Resultant image matrix after reconstruction.
     """
-    print('\nReconstructing image...')
+    logger.debug('Reconstructing image...')
     # Reconstructing original image from laplacian pyramid
     rs = laplacian[L]
     for i in range(L-1, -1, -1): 
         rs = pyramid_expand(rs, preserve_range=True)
         rs = np.add(rs, laplacian[i])
-        #print('Layer %d completed' % (i))
+        logger.debug('Layer %d completed' % (i))
+    logger.info(
+            'Finished reconstructing image from modified pyramid')
     return rs
 
 
